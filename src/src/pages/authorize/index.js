5db8a2c3e23e985c00555871ce627c8d4dae7c0f
zimingsong <zimingsong@tencent.com>
2020-06-22 16:50:52 +0800

    fix(login): 隐藏微信登录及忘记密码注册账号选项，禁用双击缩放

--- src/src/pages/authorize/index.js
+++ src/src/pages/authorize/index.js
@@ -12,7 +12,32 @@ export default function Authorize() {
       <Head>
         <title>腾讯连连</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.onload = function () {
+                document.addEventListener('touchstart', function (event) {
+                  if (event.touches.length > 1) {
+                    event.preventDefault();
+                  }
+                });
+                var lastTouchEnd = 0;
+                document.addEventListener('touchend', function (event) {
+                  var now=(new Date()).getTime();
+                  if (now - lastTouchEnd <= 300) {
+                    event.preventDefault();
+                  }
+                  lastTouchEnd = now;
+                }, false);
+                document.addEventListener('gesturestart', function (event) {
+                  event.preventDefault();
+                });
+              }
+            `,
+          }}
+        >
+        </script>
       </Head>
 
       <header>
--- src/src/pages/login/index.js
+++ src/src/pages/login/index.js
@@ -64,7 +64,32 @@ export default function Login() {
       <Head>
         <title>腾讯连连</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.onload = function () {
+                document.addEventListener('touchstart', function (event) {
+                  if (event.touches.length > 1) {
+                    event.preventDefault();
+                  }
+                });
+                var lastTouchEnd = 0;
+                document.addEventListener('touchend', function (event) {
+                  var now=(new Date()).getTime();
+                  if (now - lastTouchEnd <= 300) {
+                    event.preventDefault();
+                  }
+                  lastTouchEnd = now;
+                }, false);
+                document.addEventListener('gesturestart', function (event) {
+                  event.preventDefault();
+                });
+              }
+            `,
+          }}
+        >
+        </script>
       </Head>
 
       <main>
@@ -88,10 +113,10 @@ export default function Login() {
           )}
         </div>
 
-        <div className="extra-function">
+        {/* <div className="extra-function">
           <div className="forget-password"><a href="">忘记密码</a></div>
           <div className="register">还没有账号？<a href="">注册账号</a></div>
-        </div>
+        </div> */}
 
         <div className="optional-login">
           <div className="optional-text">
@@ -99,11 +124,11 @@ export default function Login() {
           </div>
 
           <div className="optional-btn">
-            <div className="wechat-login">
+            {/* <div className="wechat-login">
               <img src="/scf/explorerh5oauth/images/wechat.png" alt="微信"/>
-              {/* <img src="/images/wechat.png" alt="微信"/> */}
+              <img src="/images/wechat.png" alt="微信"/>
               <p>微信</p>
-            </div>
+            </div> */}
 
             {loginType === LoginType.Phone && (
               <div className="mail-login">
