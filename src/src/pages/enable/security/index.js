f0c75855877f1210db380556d63dccb4856f0da8
alexhyzheng <alexhyzheng@tencent.com>
2020-12-28 23:27:36 +0800

    feat(enable_security): add

---
+++ src/src/pages/enable/security/index.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>腾讯连连</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+      </Head>
+
+      <div>应用保障服务体系首页</div>
+    </div>
+  );
+}
