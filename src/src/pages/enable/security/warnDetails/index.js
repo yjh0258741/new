8b98e29b8ff63f38cecfb36266869babf5e58884
Your Name <you@example.com>
2021-01-04 19:53:44 +0800

    feat(security_warnDetails):预警详情页面

--- src/src/pages/enable/security/warnDetails/index.js
+++ src/src/pages/enable/security/warnDetails/index.js
@@ -4,72 +4,75 @@ import {withRouter} from 'next/router'
 import {AppGetInsuranceAlarmDetailList} from '../../../../models'
 import React, { useEffect, useState } from 'react';
 import date from '../lib/time';
-
+import store from '../rudex'
 const warnDetails=function Home({router}){
+    const policyNumber=store.getState().InsuranceNo;
+    const warnName=router.query.Name;
     const [warnDetailslist, setwarnDetails] = useState([]);
     const [warnDetailsReason, setwarnDetailsReason] = useState([]);
     const [warnDetailsSuggestion, setwarnDetailsSuggestion] = useState([]);
-    const AppGetInsuranceAlarmDetailListfun = async () => {
+    const AppGetInsuranceAlarmDetailListfun = async (InsuranceNo,Name,Offset,Limit) => {
         try {
-        return await AppGetInsuranceAlarmDetailList()
+            return await AppGetInsuranceAlarmDetailList(InsuranceNo,Name,Offset,Limit)
         } catch (e) {
-        console.log(e);
+            console.log(e);
         }
     }
     
     useEffect(()=>{
-        AppGetInsuranceAlarmDetailListfun().then((res)=>{
-        setwarnDetails(res.Alarms);
-        setwarnDetailsReason(res.Alarms[0].Reason);
-        setwarnDetailsSuggestion(res.Alarms[0].Suggestion);
+        AppGetInsuranceAlarmDetailListfun({InsuranceNo:policyNumber,Name:warnName,Offset:0,Limit:10}).then((res)=>{
+            setwarnDetails(res.Alarms);
+            setwarnDetailsReason(res.Alarms[0].Reason);
+            setwarnDetailsSuggestion(res.Alarms[0].Suggestion);
+            console.log(policyNumber);
         }).catch((err)=>{
-        console.log(err);
+            console.log(err);
         })
     },[]);
+    function toSkipWarnSet(){
+        router.push({
+            pathname:'/enable/security/warnSet',
+            query:{
+                Name:warnName
+            }
+          });
+    }
     return(
-        <div className="warn-details">
+        <div className="warnDetails">
             <Head>
             <title>腾讯连连</title>
             <link rel="icon" href="/favicon.ico" />
             <meta name="viewport" content="width=width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
             </Head> 
             <main className='warn'>
-                    <div className='warn-set'><img src='/images/seting.png' className='set-pic'/></div>
-                    <div className='warn-title'>{router.query.Name}</div>
-                    <div className='warn-main'>
-                        <div className='warn-analyse'>
-                            <div className='analyse-title'>隐患分析</div>
-                            <div className='analyse-reason'>{warnDetailsReason}</div>
+                    <div className='warnSet' onClick={toSkipWarnSet}><img src='/images/seting.png' className='setPic'/></div>
+                    <div className='warnTitle'>{warnName}</div>
+                    <div className='warnMain'>
+                        <div className='warnAnalyse'>
+                            <div className='analyseTitle'>隐患分析</div>
+                            <div className='analyseReason'>{warnDetailsReason}</div>
                         </div>
-                        <div className='warn-advise'>
-                            <div className='advise-title'>处理建议</div>
-                            <div className='cope-advise'>{warnDetailsSuggestion}</div>
+                        <div className='warnAdvise'>
+                            <div className='adviseTitle'>处理建议</div>
+                            <div className='copeAdvise'>{warnDetailsSuggestion}</div>
                         </div>
                     </div>
             </main>
-            <footer className='warn-msg'>
+            <footer className='warnMsg'>
                 <div className='title'>
-                    <div className='warn-title'>预警信息</div>
+                    <div className='warnTitle'>预警信息</div>
                     <div className='month'>
-                        <div className='month-time'>近30天</div>
+                        <div className='monthTime'>近30天</div>
                         <img src='/images/downarrow.png' /></div>
                 </div>
                 {
                     warnDetailslist.map(obj=>{
-                      return <div className='warn-content' key={obj.AlarmTimestamp}>
-                                <div className='warn-reason'>{obj.Name}</div>
-                                <div className='warn-time'>{date(obj.AlarmTimestamp)}</div>
+                      return <div className='warnContent' key={obj.AlarmTimestamp}>
+                                <div className='warnReason'>{obj.Name}</div>
+                                <div className='warnTime'>{date(obj.AlarmTimestamp)}</div>
                             </div>   
                     })
                 }
-                <div className='warn-content'>
-                    <div className='warn-reason'>电流值大于1A</div>
-                    <div className='warn-time'>2020-11-19 23:23:23</div>
-                </div>
-                <div className='warn-content'>
-                    <div className='warn-reason'>电流值大于1A</div>
-                    <div className='warn-time'>2020-11-19 23:23:23</div>
-                </div>
             </footer>
         </div>
     )
